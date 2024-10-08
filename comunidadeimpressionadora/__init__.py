8b5ce54413bbd732a16588af44bb8aab1aaf8551
Lucas Maciel <lucasboni_business@outlook.com>
2024-10-08 19:00:38 -0300

    __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'fb4dfe275e1be5f3a1c3bd37b5311b33'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -16,5 +21,3 @@ login_manager.login_message = 'Faça login para continuar'
 login_manager.login_message_category = 'alert-info'
 
 from comunidadeimpressionadora import routes
-
-
