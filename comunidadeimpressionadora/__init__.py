dcab76e62a4730428d4653291a0d60776c6ff164
LucasPyBoni <lordtsyrinxbr@gmail.com>
2024-10-21 10:47:53 -0300

    add gitignore

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -5,13 +5,9 @@ from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'fb4dfe275e1be5f3a1c3bd37b5311b33'
-
-if os.getenv("DATABASE_URL"):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['UPLOAD_FOLDER'] = 'static/fotos_perfil'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
